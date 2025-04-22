3577ce4366c867cb2157badc5bdc02eb38666451
Kaksha Patel <kaksha.patel@krishivinnovations.com>
2025-04-22 15:14:55 +0530

    resolve errors

---
+++ KHRMS.Infrastructure/Migrations/20250422082928_Added_Regularization_ApproveedBy_Request_In_EmployeeAttendance.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KHRMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Regularization_ApproveedBy_Request_In_EmployeeAttendance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "RegularizedBy",
+                table: "EmployeeAttendance",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegularizedDate",
+                table: "EmployeeAttendance",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegularizedBy",
+                table: "EmployeeAttendance");
+
+            migrationBuilder.DropColumn(
+                name: "RegularizedDate",
+                table: "EmployeeAttendance");
+        }
+    }
+}
